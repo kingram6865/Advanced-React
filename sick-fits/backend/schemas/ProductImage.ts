d4cdb5cf41c071532350c816454d64dc0b1abf2d
Ken Ingram <ken.ingram@gmail.com>
2025-04-06 13:26:42 -0700

    Completed 17 - Creating two way data relationships in Keystone

--- sick-fits/backend/schemas/ProductImage.ts
+++ sick-fits/backend/schemas/ProductImage.ts
@@ -1,13 +1,14 @@
-import { cloudinaryImage } from '@keystone-next/cloudinary'
-import { text } from '@keystone-next/fields';
+import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
+import { Product } from './Product';
 
 export const cloudinary = {
   cloudName: process.env.CLOUDINARY_CLOUD_NAME,
   apiKey: process.env.CLOUDINARY_KEY,
   apiSecret: process.env.CLOUDINARY_SECRET,
   folder: 'sickfits',
-}
+};
 
 export const ProductImage = list({
   fields: {
@@ -16,5 +17,13 @@ export const ProductImage = list({
       label: 'Source',
     }),
     altText: text(),
+    product: relationship({
+      ref: 'Product.photo',
+    }),
+  },
+  ui: {
+    listView: {
+      initialColumns: ['altText', 'image', 'product'],
+    },
   },
 });
